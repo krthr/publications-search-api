28a0173b4eca904b0a3d78524dde8aff24503a76
krthr <wtovaragudelo@gmail.com>
2023-04-09 19:19:35 +0200

    feat: Publication model & migrations

---
+++ database/factories/PublicationFactory.ts
@@ -0,0 +1,8 @@
+import Publication from 'App/Models/Publication'
+import Factory from '@ioc:Adonis/Lucid/Factory'
+
+export default Factory.define(Publication, ({}) => {
+  return {
+    //
+  }
+}).build()
--- env.ts
+++ env.ts
@@ -22,4 +22,11 @@ export default Env.rules({
   DRIVE_DISK: Env.schema.enum(['local'] as const),
   NODE_ENV: Env.schema.enum(['development', 'production', 'test'] as const),
   ML_SERVICE_URL: Env.schema.string(),
+
+  DB_CONNECTION: Env.schema.string(),
+  PG_HOST: Env.schema.string({ format: 'host' }),
+  PG_PORT: Env.schema.number(),
+  PG_USER: Env.schema.string(),
+  PG_PASSWORD: Env.schema.string.optional(),
+  PG_DB_NAME: Env.schema.string(),
 })
